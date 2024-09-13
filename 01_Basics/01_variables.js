39752e1bc5fc20fce38e520fcbe61ed33c7ca0e0
iamkeshari <biplab.rananayak@gmail.com>
2024-09-13 15:05:10 +0530

    add

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 142354
+
+ let accountEmail = 'hitesh@google.com';
+ var accountPassword = '123'; 
+ accountCity = 'Jaipur';
+
+ //accountId = 2
+ accountEmail = 'hchc@hc.com'
+ accountPassword ='123456';
+ accountCity = 'Banglore';
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
