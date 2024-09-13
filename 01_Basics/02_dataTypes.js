ea5b813ce64463db6a690a8c1a26bc8d4be24eee
iamkeshari <biplab.rananayak@gmail.com>
2024-09-13 15:02:04 +0530

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as a newer version
+
+//alert (3+3); //we are using node js, not browser
+
+console.log(3+4); console.log('Biplab');
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof 'Hitesh');
+console.log(typeof 18);
+console.log(typeof state);
