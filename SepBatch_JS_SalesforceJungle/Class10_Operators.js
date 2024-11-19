507fb9c8bef4227ff7e0726ef8d58ac1d34e26b2
iamkeshari <biplab.rananayak@gmail.com>
2024-11-20 01:03:42 +0530

    done

--- SepBatch_JS_SalesforceJungle/Class10_Operators.js
+++ SepBatch_JS_SalesforceJungle/Class10_Operators.js
@@ -1,5 +1,10 @@
-/*let a = 5;
+// Arithmatic 
+/*
+let a = 5;
 let b = 7;
+let c = '10';
+let d = 15;
+
 let add = a + b;
 let mul = a*b;
 let sub = a-b;
@@ -14,6 +19,9 @@ console.log(div);
 
 let a = 5;
 a = a + 10; 
+let c = '10';
+let d = 15;
+
 console.log(a); // 15
 
 a +=5;
@@ -26,4 +34,24 @@ a--;  //22 ---21
 console.log(a++); //20 ---21
 
 a += 10; 
-console.log(++a);  // 31  ---32
+console.log(++a);  // 31  ---
+
+console.log(c + d); //1015
+console.log(c - d); //-5
+console.log(c * d); //150
+console.log(c / d); //0.66
+
+//Assignment Operator '='/'=='/'==='
+
+//comparison Operator , > ,>=,<=,!=,==,===
+
+let myNumber = 10;
+let myNumber1 = 20;
+
+console.log(myNumber > myNumber1);
+console.log(myNumber1 > myNumber);
+
+//Logical operator (&&,||,!)
+console.log(true && false);
+console.log(false && true);
+console.log(myNumber > 0 && myNumber1 > 5);
