c1f3865df944bd1e8257c15f3289e85d822492c3
iamkeshari <biplab.rananayak@gmail.com>
2024-12-10 21:04:24 +0530

    changes

--- SepBatch_JS_SalesforceJungle/Class16_JSON.js
+++ SepBatch_JS_SalesforceJungle/Class16_JSON.js
@@ -4,10 +4,20 @@ let student3 = {'Name':'Rohan', 'Age':27,'feesPaid': 'True' , 'Street':'jajpur',
 let student4 = {'Name':'Biplab', 'Age':28,'feesPaid': 'True' , 'Street':'Puri', 'class':undefined, 'Pincode':134};
 
 students = [student1,student2,student3,student4];
-console.log(students);
+//console.log(students);
 
 let JSONStudents = JSON.stringify(students);  // Convert into JSON Format.
 console.log(JSONStudents);   
 
-let normalizeStudents = JSON.parse(JSONStudents);
+let normalizeStudents = JSON.parse(JSONStudents); // Convert to Object
 console.log(normalizeStudents);
+
+// Modify Object
+student1.dob = '01/01/1950';
+console.log('My Details',student1);
+
+myCountry = 'Country';
+student1[myCountry] = 'India';
+console.log(student1); // Dynamically add the value;
+
+
