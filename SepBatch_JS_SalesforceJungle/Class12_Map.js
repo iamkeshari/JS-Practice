243bbd7cc6d6317b14001fc253cc6a7f63939ce0
iamkeshari <biplab.rananayak@gmail.com>
2025-01-15 12:11:56 +0530

    Add Extra examples

--- SepBatch_JS_SalesforceJungle/Class12_Map.js
+++ SepBatch_JS_SalesforceJungle/Class12_Map.js
@@ -11,3 +11,54 @@ let mapStudent = students.map( str => {
 })
 
 console.log(mapStudent);
+
+console.log('Map Collections');
+/*
+method
+newMap() ----> create the Map
+map.set(Key,Values)---> stores the value by the key
+map.get(Key) -----> returns the values by the key , undefined if key doesn't exist in map.
+map.has(Key) -----> returns these if the key exists. false otherwise.
+map.delete(Key) ---> remove the element (thekey/valuepair) by the key.
+map.clear()-----> removes everything from the Map.
+map.size()-----> returns the current elememnt count.
+
+*/
+
+
+let myMap = new Map();
+console.log('myMap', myMap);
+
+// To add entries in the map
+myMap.set('1', 'Ankit');
+myMap.set(2, 50);
+myMap.set(true, 'Trainer');
+console.log('myMap', myMap);
+
+//to access the value from map
+
+console.log(myMap.get('1'));
+console.log(myMap.get(true));
+console.log(myMap.size);
+
+let favPlace = new Map([
+    ['Jaipur', 'Place'],
+    ['pune', 'fort'],
+    ['Goa', 'Beaches'],
+]);
+console.log('FavPlaces' , favPlace);
+
+//iteration
+for(let[key,value] of favPlace){
+    console.log(key,value);
+    
+}
+
+for(let currItem of favPlace.keys()){
+    console.log(currItem);
+    
+}
+for(let currItem of favPlace.values()){
+    console.log(currItem);
+    
+}
